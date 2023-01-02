15b9e5c1cd9c27f65e7673cca76b463d3d3fa333
estebanthi <estebanthi62@gmail.com>
2023-01-02 12:23:34 -0500

    Update main

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
-import yaml
 import shutil
 
 
+def move_file(path):
+    lines = []
+    with open(path, 'r', encoding='latin-1') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if "tags: zettelkasten/moc" in line:
+            new_path = 'N:\\Obsidian Vault\\100 Zettelkasten\\120 MOC\\' + path.split('\\')[-1]
+            shutil.move(path, new_path)
+            print(new_path)
+
 if __name__ == '__main__':
-    with open(os.path.join(os.path.dirname(__file__), 'config.yml')) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-
-    obsidian_vault_path = config['obsidian_vault_path']
-    obsidian_vault_path = os.path.expanduser(obsidian_vault_path)
-
-    # count the number of .md files in the vault and subfolders
-    md_files = []
-    for root, dirs, files in os.walk(obsidian_vault_path):
-        for file in files:
-            if file.endswith('.md'):
-                md_files.append(os.path.join(root, file))
-
-    print(f'Found {len(md_files)} .md files in the vault and subfolders')
+    folder_path = r"N:\Obsidian Vault\100 Zettelkasten"
+    for file in os.listdir(folder_path):
+        if file.endswith('.md'):
+            move_file(os.path.join(folder_path, file))
