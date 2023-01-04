b872a9f758cdc20ec586bfcda6acf69697cf5b66
estebanthi <estebanthi62@gmail.com>
2023-01-04 10:51:46 +0100

    Update paths

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ def move_file(path):
 
     for line in lines:
         if "tags: zettelkasten/moc" in line:
-            new_path = 'N:\\Obsidian Vault\\100 Zettelkasten\\120 MOC\\' + path.split('\\')[-1]
+            new_path = 'F:\\SHARED\\OBSIDIAN VAULT\\100 Zettelkasten\\120 MOC\\' + path.split('\\')[-1]
             shutil.move(path, new_path)
             print(new_path)
 
+
 if __name__ == '__main__':
-    folder_path = r"N:\Obsidian Vault\100 Zettelkasten"
+    folder_path = r"F:\SHARED\OBSIDIAN VAULT\100 Zettelkasten"
     for file in os.listdir(folder_path):
         if file.endswith('.md'):
             move_file(os.path.join(folder_path, file))
