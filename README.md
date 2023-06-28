112526e60cef05e318b8d37c13260ddd0ac0ddc0
estebanthi <esteban.thilliez@gmail.com>
2023-06-28 22:37:18 +0200

    Refacto repo

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # ObsidianRefactorizer
+
+ObsidianRefactorizer is a repo where I collect my scripts to interact with Obsidian.
+
+**Feel free to add yours by opening a PR!**
+
+
+## Table of contents    
+- [Tags](#tags)
+  - [add_tag_to_random_notes](#add_tag_to_random_notes)
+  - [remove_tag_from_random_notes](#remove_tag)
+
+- [General](#general)
+  - [quick_replace](#quick_replace)
+
+## Configuration
+Create a `config.yaml` file in the root of the repo. It should contain the following:
+```yaml
+vault_path: /path/to/your/vault
+```
+
+## General
+
+### quick_replace
+Replaces a regex in all notes in a specified folder.
+
+**Args**:
+- `--path`: Path to the folder where the notes are located (relative to the vault path).
+- `--regex`: Regex to replace.
+- `--new`: New string to replace the regex with.
+
+**Example**:
+```bash
+python quick_replace.py --path "my_notes" --regex "myregex" --new "mynewstring"
+```
+
+## Tags
+
+### add_tag_to_random_notes
+Adds a tag to n random notes in a specified folder.
+
+**Args**:
+- `--path`: Path to the folder where the notes are located (relative to the vault path).
+- `--n`: Number of notes to add the tag to.
+- `--tag`: Tag to add to the notes.
+
+**Example**:
+```bash
+python add_tag_to_random_notes.py --path "my_notes" --n 10 --tag "mytag"
+```
+
+### remove_tag
+Removes a tag from all notes in a specified folder.
+
+**Args**:
+- `--path`: Path to the folder where the notes are located (relative to the vault path).
+- `--tag`: Tag to remove from the notes.
+
+**Example**:
+```bash
+python remove_tag.py --path "my_notes" --tag "mytag"
+```
--- quick_replace.py
+++ quick_replace.py
@@ -1,26 +1,38 @@
 import os
 import re
+import argparse
+import yaml
 
 
-path = r"F:\SHARED\OBSIDIAN VAULT\10 Wiki\13 Plantations"
+def _get_full_path(path):
+    with open("config.yaml", 'r', encoding='utf-8') as f:
+        config = yaml.safe_load(f.read())
+    return os.path.join(config["vault_path"], path)
 
-old = "100 Zettelkasten/120 MOC/Meteorology"
-new = "Meteorology - 20230221103137|Meteorology"
 
-
-def replace_in_file(path, old, new):
+def replace_in_file(path, regex, new):
     with open(path, 'r', encoding='utf-8') as f:
         content = f.read()
-        content = content.replace(old, new)
+
+    content = re.sub(regex, new, content)
 
     with open(path, 'w', encoding='utf-8') as f:
         f.write(content)
 
 
-def replace_in_folder(path, old, new):
-    for file in os.listdir(path):
-        replace_in_file(os.path.join(path, file), old, new)
+def replace_in_folder(path, regex, new):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            replace_in_file(file_path, regex, new)
 
 
 if __name__ == "__main__":
-    replace_in_folder(path, old, new)
+    parser = argparse.ArgumentParser(description='Replace text in files.')
+    parser.add_argument('--path', type=str, help='Path to folder', required=True)
+    parser.add_argument('--regex', type=str, help='Regex to replace', required=True)
+    parser.add_argument('--new', type=str, help='New text', required=True)
+    args = parser.parse_args()
+
+    full_path = _get_full_path(args.path)
+    replace_in_folder(full_path, args.old, args.new)
